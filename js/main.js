6b47c896e932bd7bc7a33fd14bbeaf6dbbada98d
davidearbola <davidearbola@gmail.com>
2024-05-03 19:33:19 +0200

    M3-creo funzione che viene richiamata dopo un tot secondi con timer nella quale chiedo all'utente di inserire 5 numeri con 5 prompt

--- js/main.js
+++ js/main.js
@@ -14,6 +14,33 @@
 // # MILESTONE 2
 // # Impostate un timer a 3 secondi, testatelo con un console.log. Una volta funzionante fate in modo che nasconda i numeri in pagina.
 
+// // funzione che nasconde elementi in pagina
+// function nascondi(elemento) {
+// 	elemento.style.display = "none";
+// }
+// // funzione che crea numeri random
+// function numeriRandom(min, max) {
+// 	let numeroRandom = Math.floor(Math.random() * max + min);
+// 	return numeroRandom;
+// }
+
+// // variabile globale per elemento da nascondere
+// let elementoDaNascondere = document.getElementById(`text`);
+// // creo array per numeri random
+// let listaNumRandom = [];
+// // ciclo per creare 5 numeri random e li salvo in array e li stampo in pagina
+// for (let i = 1; i <= 5; i++) {
+// 	let numero = numeriRandom(1, 100);
+// 	listaNumRandom.push(numero);
+// 	document.getElementById(`text`).innerHTML = listaNumRandom;
+// }
+
+// // creo timer richiamando funzione che nasconde gli elementi
+// setTimeout(nascondi, 3000, elementoDaNascondere);
+
+// # MILESTONE 3
+// # Sempre nella funzione temporizzata, mostrate un prompt all'utente finchè non vi ritrovate con 5 numeri salvati. Poi stampate tutto l'array dei numeri utente in console.
+
 // funzione che nasconde elementi in pagina
 function nascondi(elemento) {
 	elemento.style.display = "none";
@@ -24,6 +51,17 @@ function numeriRandom(min, max) {
 	return numeroRandom;
 }
 
+// funzione che chiede numeri per tot volte ad utente e li salva in array
+function chiediNumeri(numeriUtente) {
+	for (let i = 1; i <= 5; i++) {
+		let numeroUtente = prompt(`Scrivi qui il numero che ricordi`);
+		numeriUtente.push(numeroUtente);
+	}
+	console.log(numeriUtente);
+}
+
+// creo array per numeri utente
+let numeriUser = [];
 // variabile globale per elemento da nascondere
 let elementoDaNascondere = document.getElementById(`text`);
 // creo array per numeri random
@@ -35,5 +73,10 @@ for (let i = 1; i <= 5; i++) {
 	document.getElementById(`text`).innerHTML = listaNumRandom;
 }
 
+console.log(listaNumRandom);
+
 // creo timer richiamando funzione che nasconde gli elementi
 setTimeout(nascondi, 3000, elementoDaNascondere);
+
+// creo timer per far apparire i prompt
+setTimeout(chiediNumeri, 3100, numeriUser);
