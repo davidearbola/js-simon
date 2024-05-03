a869df08c788f0e29bdba258e7aef3166088ad5f
davidearbola <davidearbola@gmail.com>
2024-05-03 19:01:33 +0200

    M1-genero 5 numeri casuali da 1 a 100 con una funzione, li stampo in pagina grazie al ciclo e li salvo in array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log(`ciao`);
+// # MILESTONE 1
+// # Generate 5 numeri casuali. Può tornarvi utile una funzione? Provate a stamparli prima in console e poi in pagina.
+// # :warning: Ricordate che questi numeri dovrete ricontrollarli più avanti quindi non "buttateli" ma conservateli da qualche parte
+
+// funzione che crea numeri random
+let listaNumRandom = [];
+// ciclo per creare 5 numeri random e li salvo in array e li stampo in pagina
+for (let i = 1; i <= 5; i++) {
+	let numero = numeriRandom(1, 100);
+	listaNumRandom.push(numero);
+	document.getElementById(`text`).innerHTML = listaNumRandom;
+}
+
+function numeriRandom(min, max) {
+	let numeroRandom = Math.floor(Math.random() * max + min);
+	return numeroRandom;
+}
