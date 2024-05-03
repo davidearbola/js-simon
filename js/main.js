c2c9ac4c788f67892ff1979ff7b1dd1c02b17210
davidearbola <davidearbola@gmail.com>
2024-05-03 19:18:08 +0200

    M2-creo funzione che nasconde elementi e la richiamo all'interno di un timer

--- js/main.js
+++ js/main.js
@@ -2,7 +2,31 @@
 // # Generate 5 numeri casuali. Può tornarvi utile una funzione? Provate a stamparli prima in console e poi in pagina.
 // # :warning: Ricordate che questi numeri dovrete ricontrollarli più avanti quindi non "buttateli" ma conservateli da qualche parte
 
+// // funzione che crea numeri random
+// let listaNumRandom = [];
+// // ciclo per creare 5 numeri random e li salvo in array e li stampo in pagina
+// for (let i = 1; i <= 5; i++) {
+// 	let numero = numeriRandom(1, 100);
+// 	listaNumRandom.push(numero);
+// 	document.getElementById(`text`).innerHTML = listaNumRandom;
+// }
+
+// # MILESTONE 2
+// # Impostate un timer a 3 secondi, testatelo con un console.log. Una volta funzionante fate in modo che nasconda i numeri in pagina.
+
+// funzione che nasconde elementi in pagina
+function nascondi(elemento) {
+	elemento.style.display = "none";
+}
 // funzione che crea numeri random
+function numeriRandom(min, max) {
+	let numeroRandom = Math.floor(Math.random() * max + min);
+	return numeroRandom;
+}
+
+// variabile globale per elemento da nascondere
+let elementoDaNascondere = document.getElementById(`text`);
+// creo array per numeri random
 let listaNumRandom = [];
 // ciclo per creare 5 numeri random e li salvo in array e li stampo in pagina
 for (let i = 1; i <= 5; i++) {
@@ -11,7 +35,5 @@ for (let i = 1; i <= 5; i++) {
 	document.getElementById(`text`).innerHTML = listaNumRandom;
 }
 
-function numeriRandom(min, max) {
-	let numeroRandom = Math.floor(Math.random() * max + min);
-	return numeroRandom;
-}
+// creo timer richiamando funzione che nasconde gli elementi
+setTimeout(nascondi, 3000, elementoDaNascondere);
